f4d9e67d2886c2067acb7c71f702dfd63bfc9c19
Mehdi <elmehdiworkemail@gmail.com>
2024-10-28 22:15:33 +0000

    adding exceptions to the auth

--- src/main/java/com/spring/huntersleague/service/dto/UserLoginDTO.java
+++ src/main/java/com/spring/huntersleague/service/dto/UserLoginDTO.java
@@ -3,20 +3,20 @@ package com.spring.huntersleague.service.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class UserLoginDTO {
 
     @NotBlank(message = "Username is required")
     private String username;
 
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
-            message = "Password must contain at least one uppercase letter, one lowercase letter, and one digit")
+    @Size(min =8, message = "Password must be at least 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d).+$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, and one digit ")
     private String password;
 }
